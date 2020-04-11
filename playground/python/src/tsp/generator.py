660646ae23c502b82b30cc2c34755b8789986d2f
dmitry.plescheev <dmitry.plescheev@yandex.ru>
2020-04-11 03:49:02 +0300

    generator for tsp

--- playground/python/src/tsp/generator.py
+++ playground/python/src/tsp/generator.py
@@ -3,7 +3,5 @@ from typing import Tuple
 
 
 def generator(count, min_x=0.0, max_x=100.0, min_y=0.0, max_y=100.0) -> Tuple[float, float]:
-    temp = 0
-    while temp < count:
-        temp += 1
+    for _ in range(count):
         yield uniform(min_x, max_x), uniform(min_y, max_y)
